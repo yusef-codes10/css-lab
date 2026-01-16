c475af81fb1e625d63faf0eaf99b26ae8da565e5
Youcef <youcefcopy@gmail.com>
2026-01-16 11:31:37 +0100

    cards.js to add card dynamically

---
+++ cards.js
@@ -0,0 +1,12 @@
+// adding the cards dynamically is way better
+const cards = [
+    {
+        id: 'card1',
+        langsUsed: ['html', 'css'],
+        cardTitle: 'Retro Button',
+        cardDiscription: 'View a cool retro button'
+    }, 
+    {
+
+    }
+]
