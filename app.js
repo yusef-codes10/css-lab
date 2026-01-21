a5c01698e8ba2b7f61d4f77da1804f7cc32d331a
Youcef <youcefcopy@gmail.com>
2026-01-21 18:56:49 +0100

    setting up loadContent()

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log('js has been loaded');
 
 // 1- load the cards components
 // 2- navigate through cards contents
+
+function loadContent() {
+    // creating the divs, loading cards.js content here
+}
