add717c4c1a888cfa0365a5c61d49b96b9fb4594
Youcef <youcefcopy@gmail.com>
2026-01-21 18:58:59 +0100

    declaring the main container

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ console.log('js has been loaded');
 // 1- load the cards components
 // 2- navigate through cards contents
 
+const componentSection = document.querySelector('.component-section');
+
 function loadContent() {
     // creating the divs, loading cards.js content here
 }
