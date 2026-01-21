76ed46ab106c06281a386aad4c6f1f91be41df30
Youcef <youcefcopy@gmail.com>
2026-01-21 19:14:40 +0100

    using the foreach()

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const componentSection = document.querySelector('.component-section');
 
 function loadContent() {
     // creating the divs, loading cards.js content here
+    cards.forEach(
+        card => {
 
-
+        }
+    )
 }
