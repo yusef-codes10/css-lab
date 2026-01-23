acfe759b4077fe9dd4e4102ec147a1d1cbff9c0d
Youcef <youcefcopy@gmail.com>
2026-01-23 10:34:13 +0100

    adding the gridArea as a property in the card object literal

--- cards.js
+++ cards.js
@@ -4,37 +4,43 @@ const cards = [
         id: 'card1',
         langsUsed: ['html', 'css'],
         cardTitle: 'Retro Button',
-        cardDiscription: 'View a cool retro button'
+        cardDiscription: 'View a cool retro button',
+        girdArea: 'button'
     }, 
     {
         id: 'navbar1',
         langsUsed: ['html', 'css'],
         cardTitle: 'Navbar',
-        cardDiscription: 'Modern navabr layout'
+        cardDiscription: 'Modern navabr layout',
+        girdArea: 'animation'
     },
     {
         id: 'navbar2',
         langsUsed: ['html', 'css', 'js'],
         cardTitle: 'Navbar',
-        cardDiscription: 'Responsive Navbar'
+        cardDiscription: 'Responsive Navbar',
+        girdArea: 'icon'
     }, 
     {
         id: 'animation',
         langsUsed: ['css'],
         cardTitle: 'Animation',
-        cardDiscription: 'View a variety of awesome CSS3 Animations that you can edit and slot right into your next website!'
+        cardDiscription: 'View a variety of awesome CSS3 Animations that you can edit and slot right into your next website!',
+        girdArea: 'textinput'
     },
     {
         id: 'text-input',
         langsUsed: ['html', 'css'],
         cardTitle: 'Text Input',
-        cardDiscription: 'View a selection of text input forms ranging from text inputs, textarea\'s, radio buttons, and custom dropdown menus. Many components include custom focus, hover & active states!'
+        cardDiscription: 'View a selection of text input forms ranging from text inputs, textarea\'s, radio buttons, and custom dropdown menus. Many components include custom focus, hover & active states!',
+        girdArea: 'modal'
     },
     {
         id: 'miscellaneous',
         langsUsed: ['html', 'css', 'js'],
         cardTitle: 'Miscellaneous',
-        cardDiscription: 'A random selection of components that didn\'t quite fit in any of the other categories!'
+        cardDiscription: 'A random selection of components that didn\'t quite fit in any of the other categories!',
+        girdArea: 'miscellaneous'
     }
 ]
 
